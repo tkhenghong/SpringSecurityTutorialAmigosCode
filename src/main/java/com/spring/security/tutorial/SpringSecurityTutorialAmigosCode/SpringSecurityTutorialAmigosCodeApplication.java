7d099dee5a6609872d8b2d273c7860e1bd9c4ed6
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2020-03-09 12:20:41 +0800

    Init commit

---
+++ src/main/java/com/spring/security/tutorial/SpringSecurityTutorialAmigosCode/SpringSecurityTutorialAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.tutorial.SpringSecurityTutorialAmigosCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutorialAmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTutorialAmigosCodeApplication.class, args);
+	}
+
+}
